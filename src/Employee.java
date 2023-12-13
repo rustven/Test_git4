da635cb7293e7641ec22253386211b1adaa3bf42
Andrej Rusetski <rustven@gmail.com>
2023-12-13 20:28:39 +0100

    add Employee class

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee extends Peron {
+
+private double salary;
+
+    public Employee(String firstName, String lastName, double salary) {
+        super(firstName, lastName);
+        this.salary = salary;
+    }
+
+}
+
